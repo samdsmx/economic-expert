df5fb7fc1440723c1b7ed40f6af87c54f5cfaba6
Sergio Marquez <semarque@microsoft.com>
2023-05-22 08:14:39 -0700

    remove particles

--- src/Pages/AboutPage.tsx
+++ src/Pages/AboutPage.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Particles from "react-tsparticles";
-import image from "../Resources/code.jpg";
+// import image from "../Resources/code.jpg";
 import uaz from "../Resources/UAZ.jpg";
 import unadm from "../Resources/unadm.png";
 import marlen from "../Resources/marlen.jpg";
@@ -8,102 +7,9 @@ import imelda from "../Resources/imelda.jpg";
 import sergio from "../Resources/sergio.jpg";
 import hector from "../Resources/hector.jpg";
 import female from "../Resources/female.jpg";
-import { loadFull } from "tsparticles";
 import { Stack } from "@fluentui/react";
 
-import { Engine, IOptions, RecursivePartial } from "tsparticles-engine";
-
 function AboutPage() {
-
-    const particlesInit = async (main: Engine) => {
-        await loadFull(main);
-    };
-
-
-    const particlesOptions: RecursivePartial<IOptions> = {
-        background: {
-            image: `url(${image})`,
-            color: {
-                value: "#555555",
-            },
-            size: "cover",
-            opacity: 0,
-        },
-        fullScreen: {
-            enable: true,
-            zIndex: -1,
-        },
-        fpsLimit: 120,
-        interactivity: {
-
-            events: {
-                onClick: {
-                    enable: true,
-                    mode: "push",
-                },
-                onHover: {
-                    enable: true,
-                    mode: "grab"
-                },
-
-                resize: true,
-            },
-            modes: {
-                push: {
-                    quantity: 4,
-                },
-                grab: {
-                    distance: 200,
-                    lineLinked: {
-                        opacity: 0.5
-                    }
-                }
-
-            },
-        },
-        particles: {
-            color: {
-                value: "#ffffff",
-            },
-            links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-            },
-            collisions: {
-                enable: true,
-            },
-            move: {
-                direction: "none",
-                enable: true,
-                outModes: {
-                    default: "bounce",
-                },
-                random: false,
-                speed: 1,
-                straight: false,
-            },
-            number: {
-                density: {
-                    enable: true,
-                    area: 800,
-                },
-                value: 80,
-            },
-            opacity: {
-                value: 0.5,
-            },
-            shape: {
-                type: "circle",
-            },
-            size: {
-                value: { min: 1, max: 5 },
-            },
-        },
-        detectRetina: true,
-    };
 
     const imgStyle = {
         width: `150px`,
@@ -225,7 +131,6 @@ function AboutPage() {
                     </Stack>
                 </Stack>
             </Stack>
-            <Particles id="tsparticles" init={particlesInit} options={particlesOptions} />
         </React.Fragment>
     );
 }
