e58c0fe83c8865f9dbcafdb5e351c6b81b7e428c
Sergio Marquez <semarque@microsoft.com>
2023-05-23 11:29:03 -0700

    filter null

--- src/Hooks/UseAPIData.tsx
+++ src/Hooks/UseAPIData.tsx
@@ -12,7 +12,7 @@ export function useAPIData() {
       .get(`https://economic-expert-default-rtdb.firebaseio.com/data.json`)
       .then((response) => {
         console.log(response);
-        setAPIData(response.data);
+        setAPIData(response.data.filter(item => item !== null));
         setLoading(false);
       })
       .catch(error => {
