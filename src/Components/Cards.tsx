9c0342bebe2afabe872e7eb3fd10f3392f064c77
Sergio Marquez <semarque@microsoft.com>
2023-05-23 00:06:30 -0700

    10

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -5,9 +5,9 @@ import { Card } from "./Card";
 export function Cards({ items }) {
   return (
     <Stack horizontal style={{ display: "flex", flexFlow: "wrap" }}>
-      {items.slice(0,1).map((item, index) => {
-          console.log(`items`);
-          console.log(items);
+      {items.slice(0,10).map((item, index) => {
+        console.log(`items`);
+        console.log(items);
         return (
           <Card 
             key={index} 
